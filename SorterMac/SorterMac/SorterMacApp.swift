807bfb8e4787a47a1485785d32489948dc75304b
kmw4561@gmail.com <kmw4561@gmail.com>
2025-07-30 16:36:51 +0900

    initial commit

---
+++ SorterMac/SorterMac/SorterMacApp.swift
@@ -0,0 +1,32 @@
+//
+//  SorterMacApp.swift
+//  SorterMac
+//
+//  Created by 김민우 on 7/30/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct SorterMacApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
