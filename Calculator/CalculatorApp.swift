cb98149f5e21db268b485f7552d44c1f63ba31df
kmw4561@gmail.com <kmw4561@gmail.com>
2025-08-05 15:19:49 +0900

    add Calculator iOS project

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by 김민우 on 8/5/25.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
